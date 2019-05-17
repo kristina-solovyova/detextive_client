b361f0c3259332767c45eba5e907a6b975874446
Kristina Solovyova <kris.solo15@gmail.com>
2019-05-17 04:49:35 +0300

    Update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,87 +1,50 @@
 <template>
   <mdb-container>
-    <h2 class="h1-responsive font-weight-bold text-center my-5">Recent posts</h2>
+    <h2 class="h1-responsive font-weight-bold text-center my-5">Welcome to {{ title }}!</h2>
     <p class="text-center w-responsive mx-auto mb-5">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <mdb-row>
       <mdb-col lg="5">
-        <mdb-view class="rounded z-depth-2 mb-lg-0 mb-4" hover>
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg" alt="Sample image" />
+        <mdb-view class="rounded mb-lg-0 mb-4" hover>
+          <img class="img-fluid" src="https://rietveld-ict.nl/wp-content/uploads/2014/01/users.png" alt="Users image" />
           <a>
             <mdb-mask overlay="white-slight" waves/>
           </a>
         </mdb-view>
       </mdb-col>
       <mdb-col lg="7">
-        <a class="green-text">
+        <a class="deep-purple-text">
           <h6 class="font-weight-bold mb-3">
-            <mdb-icon icon="utensils" class="pr-2" />Food</h6>
+            <mdb-icon icon="user" class="pr-2" />New users every day</h6>
         </a>
         <h3 class="font-weight-bold mb-3 p-0">
-          <strong>Title of the news</strong>
+          <strong>The most attractive app of the year</strong>
         </h3>
         <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat mdb-iconcere
           possimus, omnis voluptas assumenda est, omnis dolor repellendus et aut officiis debitis.</p>
-        <p>by
-          <a>
-            <strong>Carine Fox</strong>
-          </a>, 19/08/2018</p>
-        <mdb-btn color="success" size="md" class="waves-light ">Read more</mdb-btn>
       </mdb-col>
     </mdb-row>
     <hr class="my-5" />
     <mdb-row>
       <mdb-col lg="7">
+        <h3 class="font-weight-bold mb-3 p-0">
+          <strong>Text extraction has never been so easy</strong>
+        </h3>
         <a class="pink-text">
           <h6 class="font-weight-bold mb-3">
-            <mdb-icon icon="image" class="pr-2" />Lifestyle
+            <mdb-icon icon="lightbulb" class="pr-2" />Based on new algorithm
           </h6>
         </a>
-        <h3 class="font-weight-bold mb-3 p-0">
-          <strong>Title of the news</strong>
-        </h3>
         <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
           quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
-        <p>by
-          <a>
-            <strong>Carine Fox</strong>
-          </a>, 14/08/2018</p>
-        <mdb-btn color="pink" size="md" class="mb-lg-0 mb-4 waves-light">Read more</mdb-btn>
       </mdb-col>
       <mdb-col lg="5">
         <mdb-view class="rounded z-depth-2 mb-lg-0 mb-4" hover>
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/img%20(34).jpg" alt="Sample image" />
+          <img class="img-fluid" src="https://firebase.google.com/docs/ml-kit/images/text_recognition@2x.png" alt="Text extraction" />
           <a>
             <mdb-mask overlay="white-slight" waves/>
           </a>
         </mdb-view>
-      </mdb-col>
-    </mdb-row>
-    <hr class="my-5" />
-    <mdb-row>
-      <mdb-col lg="5">
-        <mdb-view class="rounded z-depth-2 mb-lg-0 mb-4" hover>
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/img (28).jpg" alt="Sample image" />
-          <a>
-            <mdb-mask overlay="white-slight" waves/>
-          </a>
-        </mdb-view>
-      </mdb-col>
-      <mdb-col lg="7">
-        <a class="indigo-text">
-          <h6 class="font-weight-bold mb-3">
-            <mdb-icon icon="suitcase" class="pr-2" />Food</h6>
-        </a>
-        <h3 class="font-weight-bold mb-3 p-0">
-          <strong>Title of the news</strong>
-        </h3>
-        <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Neque porro qui dolorem ipsum quia sit amet.</p>
-        <p>by
-          <a>
-            <strong>Carine Fox</strong>
-          </a>, 11/08/2018</p>
-        <mdb-btn color="indigo" size="md" class="waves-light">Read more</mdb-btn>
       </mdb-col>
     </mdb-row>
   </mdb-container>
@@ -99,6 +62,11 @@ export default {
     mdbIcon,
     mdbView,
     mdbBtn
+  },
+  data() {
+    return {
+      title: process.env.VUE_APP_TITLE
+    };
   }
 };
 </script>
